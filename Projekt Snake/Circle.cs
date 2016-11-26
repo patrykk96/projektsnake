bab9207f9e41e8ad48863fd0697667bad1a8e696
Patryk Kucinski <rambotrix@gmail.com>
2016-11-26 15:53:19 +0100

    Rozbudowa panelu z polem gry, dodanie klasy, z której powstanie wąż i pożywienie.

---
+++ Projekt Snake/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Snake
+{
+    class Circle
+    {
+        //Każda jednostka jedzenia bedzie kołem, a wąż będzie się składał z listy kół
+        public int x;
+        public int y;
+
+        public Circle()
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+}
--- Projekt Snake/formSnakeGame.Designer.cs
+++ Projekt Snake/formSnakeGame.Designer.cs
@@ -35,8 +35,12 @@
             this.buttonCredits = new System.Windows.Forms.Button();
             this.buttonHowToPlay = new System.Windows.Forms.Button();
             this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBoxGameField = new System.Windows.Forms.PictureBox();
+            this.labelScoreString = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGameField)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -45,25 +49,27 @@
             this.panelMenu.Controls.Add(this.buttonCredits);
             this.panelMenu.Controls.Add(this.buttonHowToPlay);
             this.panelMenu.Controls.Add(this.buttonStart);
-            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Location = new System.Drawing.Point(68, 12);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(529, 477);
+            this.panelMenu.Size = new System.Drawing.Size(870, 700);
             this.panelMenu.TabIndex = 0;
             // 
             // panelGame
             // 
+            this.panelGame.Controls.Add(this.label1);
+            this.panelGame.Controls.Add(this.labelScoreString);
+            this.panelGame.Controls.Add(this.pictureBoxGameField);
             this.panelGame.Controls.Add(this.buttonBackToMenu);
-            this.panelGame.Controls.Add(this.panelMenu);
-            this.panelGame.Location = new System.Drawing.Point(116, 12);
+            this.panelGame.Location = new System.Drawing.Point(68, 12);
             this.panelGame.Name = "panelGame";
-            this.panelGame.Size = new System.Drawing.Size(529, 477);
+            this.panelGame.Size = new System.Drawing.Size(870, 700);
             this.panelGame.TabIndex = 4;
             // 
             // buttonBackToMenu
             // 
-            this.buttonBackToMenu.Location = new System.Drawing.Point(367, 13);
+            this.buttonBackToMenu.Location = new System.Drawing.Point(728, 662);
             this.buttonBackToMenu.Name = "buttonBackToMenu";
-            this.buttonBackToMenu.Size = new System.Drawing.Size(142, 23);
+            this.buttonBackToMenu.Size = new System.Drawing.Size(142, 35);
             this.buttonBackToMenu.TabIndex = 0;
             this.buttonBackToMenu.Text = "Powrót do menu";
             this.buttonBackToMenu.UseVisualStyleBackColor = true;
@@ -71,7 +77,7 @@
             // 
             // buttonExit
             // 
-            this.buttonExit.Location = new System.Drawing.Point(177, 279);
+            this.buttonExit.Location = new System.Drawing.Point(346, 441);
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(163, 52);
             this.buttonExit.TabIndex = 3;
@@ -81,7 +87,7 @@
             // 
             // buttonCredits
             // 
-            this.buttonCredits.Location = new System.Drawing.Point(177, 221);
+            this.buttonCredits.Location = new System.Drawing.Point(346, 371);
             this.buttonCredits.Name = "buttonCredits";
             this.buttonCredits.Size = new System.Drawing.Size(163, 52);
             this.buttonCredits.TabIndex = 2;
@@ -91,7 +97,7 @@
             // 
             // buttonHowToPlay
             // 
-            this.buttonHowToPlay.Location = new System.Drawing.Point(177, 163);
+            this.buttonHowToPlay.Location = new System.Drawing.Point(346, 301);
             this.buttonHowToPlay.Name = "buttonHowToPlay";
             this.buttonHowToPlay.Size = new System.Drawing.Size(163, 52);
             this.buttonHowToPlay.TabIndex = 1;
@@ -101,7 +107,7 @@
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(177, 105);
+            this.buttonStart.Location = new System.Drawing.Point(346, 231);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(163, 52);
             this.buttonStart.TabIndex = 0;
@@ -109,12 +115,42 @@
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
+            // pictureBoxGameField
+            // 
+            this.pictureBoxGameField.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBoxGameField.Location = new System.Drawing.Point(18, 15);
+            this.pictureBoxGameField.Name = "pictureBoxGameField";
+            this.pictureBoxGameField.Size = new System.Drawing.Size(631, 661);
+            this.pictureBoxGameField.TabIndex = 1;
+            this.pictureBoxGameField.TabStop = false;
+            // 
+            // labelScoreString
+            // 
+            this.labelScoreString.AutoSize = true;
+            this.labelScoreString.Font = new System.Drawing.Font("BankGothic Lt BT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScoreString.Location = new System.Drawing.Point(695, 15);
+            this.labelScoreString.Name = "labelScoreString";
+            this.labelScoreString.Size = new System.Drawing.Size(120, 30);
+            this.labelScoreString.TabIndex = 2;
+            this.labelScoreString.Text = "Punkty";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("BankGothic Lt BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(714, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
             // formSnakeGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.ClientSize = new System.Drawing.Size(1006, 721);
             this.Controls.Add(this.panelGame);
+            this.Controls.Add(this.panelMenu);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "formSnakeGame";
@@ -122,6 +158,8 @@
             this.Load += new System.EventHandler(this.formSnakeGame_Load);
             this.panelMenu.ResumeLayout(false);
             this.panelGame.ResumeLayout(false);
+            this.panelGame.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGameField)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,5 +173,8 @@
         private System.Windows.Forms.Button buttonCredits;
         private System.Windows.Forms.Button buttonHowToPlay;
         private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelScoreString;
+        private System.Windows.Forms.PictureBox pictureBoxGameField;
     }
 }
