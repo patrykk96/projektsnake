05c1a099c7fdaaaa73dcb79f4617ce6f1a40588a
Patryk Kucinski <rambotrix@gmail.com>
2016-11-26 19:04:35 +0100

    Dodanie klasy settings, ktorej pola beda obslugiwac gre

---
+++ Projekt Snake/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Snake
+{
+    class Settings
+    {
+        //ustawienia pola gry, predkosc, polozenie , zakonczenie gry, punktacja
+        public static int width;
+        public static int height;
+        public static int score;
+        public static int speed;
+        public static bool gameOver;
+
+        public Settings()
+        {
+            //ustawienia domyślne wartości do zmienienia kiedy będzie można sprawdzić je na polu gry
+            width = 0;
+            height = 0;
+            score = 0;
+            speed = 0;
+            gameOver = false;
+
+        }
+
+    }
+}
